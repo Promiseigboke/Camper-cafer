a22206ead4a9a6819947351a1deeb1f5d0235497
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-07-06 05:15:54 +0100

    I restarted the camper-cafe project by doing a cafe house project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./*.html"], // Update the content path to match your HTML file location
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
