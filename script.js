cf664a18f64b125d0ba59f6bc9a2e41de4d10be2
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-11-09 06:00:31 +0100

    I started making the preloader

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+// Hide the preloader and show the content.
+document.getElementById("preloader").style.display = "none";
+document.getElementById("content").classList.remove("hidden");
+}, 2000)
