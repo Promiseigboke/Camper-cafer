7633aa8309eac8195a1049c4d24cb707a87a82d4
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-11-11 23:23:50 +0100

    futile

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ setTimeout(function() {
 // Hide the preloader and show the content.
 document.querySelector(".preloader").style.display = "none";
 document.getElementById("content").classList.remove("hidden");
-}, 3000)
+}, 100000)
