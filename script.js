ec512ef0467714db296a422c87a99b2f7ddc1e01
Promiseigboke <igbokesomtochukwupromise@gmail.com>
2023-12-27 00:07:49 +0100

    i started making it responsive

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ setTimeout(function() {
 // Hide the preloader and show the content.
 document.querySelector(".preloader").style.display = "none";
 document.getElementById("content").classList.remove("hidden");
-}, 100000)
+}, 1000)
